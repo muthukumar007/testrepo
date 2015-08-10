a8d19066b3e2fda641f6cf610f0918ba6437a30c
karthick <karthick.g@psiog.com>
2015-08-10 18:32:00 +0530

    Commited by Karthick

--- OwinSample/WebApplication1/AuthContext.cs
+++ OwinSample/WebApplication1/AuthContext.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+// Changes done by Karthick.G
 namespace OwinPOC
 {
     public class AuthContext : IdentityDbContext<IdentityUser>
